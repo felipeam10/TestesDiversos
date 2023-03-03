35fe0a0dd714bca146491cb2aabc7e1f78619dae
felipeam10 <felipeam10@hotmail.com>
2023-03-03 01:33:59 -0300

    commit aulas 57,58,59,60

---
+++ src/exerciciosNelioAlves/Aula571.java
@@ -0,0 +1,23 @@
+package exerciciosNelioAlves;
+
+import java.util.Scanner;
+
+public class Aula571 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //89 = false
+        //113 = true
+
+        int mask = 0b00100000; // 32
+        int n = sc.nextInt();
+
+        if((n & mask) != 0) {
+            System.out.println("6th bit is true");
+        } else {
+            System.out.println("6th bit is false");
+        }
+
+    }
+}
