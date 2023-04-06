ecd3af6632627f8a5d6d8210e584f601519bec62
felipeam10 <felipeam10@hotmail.com>
2023-04-06 01:06:48 -0300

    aula78 - Encapsulamento

---
+++ src/exerciciosNelioAlves/aula78/entities/Product.java
@@ -0,0 +1,63 @@
+package exerciciosNelioAlves.aula78.entities;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {}
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double totalValueInStock() {
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity -= quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{ " +
+                "name=' " + name + '\'' +
+                String.format("%.2f", price) +
+                ", quantity= " + quantity +
+                ", Total: $" + String.format("%.2f", totalValueInStock()) +
+                " }";
+    }
+}
