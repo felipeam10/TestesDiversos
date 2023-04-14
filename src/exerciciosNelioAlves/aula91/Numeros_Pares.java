0b38072e8ce2436a71ab886a00234500b4ebe8df
felipeam10 <felipeam10@hotmail.com>
2023-04-14 14:11:16 -0300

    aula91 - numeros pares

--- src/exerciciosNelioAlves/aula91/Numeros_Pares.java
+++ src/exerciciosNelioAlves/aula91/Numeros_Pares.java
@@ -1,2 +1,36 @@
-package exerciciosNelioAlves.aula91;public class Numeros_Pares {
+package exerciciosNelioAlves.aula91;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Numeros_Pares {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+
+        int number[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um numero: ");
+            number[i] = sc.nextInt();
+        }
+
+        //Exibindo todos os elementos do vetor
+        int qdePares = 0;
+        System.out.println("\n\nNUMEROS PARES: ");
+        for (int i=0; i<n; i++) {
+            if(number[i] % 2 == 0) {
+                System.out.printf("%2d  ", number[i]);
+                qdePares++;
+            }
+        }
+
+        System.out.printf("\n\nQUANTIDADE DE PARES = %d\n", qdePares);
+
+        sc.close();
+    }
 }
