2fe3cdd36f55f0cd2f2bf49dc58e6466a9b6369c
felipeam10 <felipeam10@hotmail.com>
2023-04-20 12:24:44 -0300

    aula95 - boxing, unboxing e wrapper classes (casting)

---
+++ src/exerciciosNelioAlves/aula95/Aula95.java
@@ -0,0 +1,18 @@
+package exerciciosNelioAlves.aula95;
+
+public class Aula95 {
+
+    public static void main(String[] args) {
+
+        //casting
+        int x = 20;
+//        Object obj = x;
+        Integer obj = x;
+        System.out.println(obj);
+
+//        int y = (int) obj;
+        int y = obj * 2;
+        System.out.println(y);
+
+    }
+}
