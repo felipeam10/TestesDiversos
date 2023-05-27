cd4ed220229d8b24a0684719ddc6d5e1b6117a41
felipeam10 <felipeam10@hotmail.com>
2023-05-27 01:19:01 -0300

    reading text file with the File and Scanner classes

---
+++ src/exerciciosNelioAlves/aula214/Program.java
@@ -0,0 +1,25 @@
+package exerciciosNelioAlves.aula214;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\DELL\\workspace-projects\\TestesDiversos\\src\\resources\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if(sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
