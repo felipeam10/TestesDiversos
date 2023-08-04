da85ed178005d2aeec19a72c4f95638c427e1158
felipeam10 <felipeam10@hotmail.com>
2023-08-04 01:23:17 -0300

    aula163 Classes abstratas

---
+++ src/exerciciosNelioAlves/aula163/entities/SavingsAccountPlus.java
@@ -0,0 +1,9 @@
+package exerciciosNelioAlves.aula163.entities;
+
+public class SavingsAccountPlus extends SavingsAccount {
+
+//    @Override
+//    public void withdraw(Double amount) {
+//        balance -= amount;
+//    }
+}
