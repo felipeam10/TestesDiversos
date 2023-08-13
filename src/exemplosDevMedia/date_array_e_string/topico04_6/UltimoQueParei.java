a871351fd0101dc67a9a9dc9da043053dd56dfd9
felipeam10 <felipeam10@hotmail.com>
2023-08-13 01:53:40 -0300

    v4.6

---
+++ src/exemplosDevMedia/date_array_e_string/topico04_6/UltimoQueParei.java
@@ -0,0 +1,9 @@
+package exemplosDevMedia.date_array_e_string.topico04_6;
+
+public class UltimoQueParei {
+
+    /**
+     *  Para aperfeiçoar a utilização das Strings, uma boa prática é valer-se de um artifício
+     *  Fazer um control + F
+     */
+}
