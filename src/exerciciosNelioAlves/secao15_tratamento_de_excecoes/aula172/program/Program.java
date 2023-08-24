8580f4caf6fe8a9b82bc4780f9028d529b54ce4f
felipeam10 <felipeam10@hotmail.com>
2023-08-24 02:29:56 -0300

    aula172 - block finally

--- src/exerciciosNelioAlves/secao15_tratamento_de_excecoes/aula172/program/Program.java
+++ src/exerciciosNelioAlves/secao15_tratamento_de_excecoes/aula172/program/Program.java
@@ -1,2 +1,27 @@
-package exerciciosNelioAlves.secao15_tratamento_de_excecoes.aula172.program;public class Program {
+package exerciciosNelioAlves.secao15_tratamento_de_excecoes.aula172.program;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+    }
 }
