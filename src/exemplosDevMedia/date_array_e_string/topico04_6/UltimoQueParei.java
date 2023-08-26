56243b5a444951ea4910e033760f0c98d8c93b05
felipeam10 <felipeam10@hotmail.com>
2023-08-26 06:02:44 -0300

    v4.6 String: trabalhando com texto em java

--- src/exemplosDevMedia/date_array_e_string/topico04_6/UltimoQueParei.java
+++
@@ -1,9 +0,0 @@
-package exemplosDevMedia.date_array_e_string.topico04_6;
-
-public class UltimoQueParei {
-
-    /**
-     *  Para aperfeiçoar a utilização das Strings, uma boa prática é valer-se de um artifício
-     *  Fazer um control + F
-     */
-}
---
+++ src/exemplosDevMedia/date_array_e_string/topico04_7/UltimoQueParei.java
@@ -0,0 +1,10 @@
+package exemplosDevMedia.date_array_e_string.topico04_7;
+
+public class UltimoQueParei {
+
+    /**
+     *  Começar o artigo 4.7
+     *  String em Java: Entendendo e utilizando essa classe
+     *
+     */
+}
