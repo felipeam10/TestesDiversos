b8cb34de94adf4466cdc5bc43033022cd51d9517
felipeam10 <felipeam10@hotmail.com>
2023-09-29 17:24:53 -0300

    v4.7 - how to create a string

---
+++ src/exemplosDevMedia/date_array_e_string/topico04_7/Pessoa.java
@@ -0,0 +1,18 @@
+package exemplosDevMedia.date_array_e_string.topico04_7;
+
+class Pessoa {
+    String nome;
+}
+
+class PessoaTeste {
+
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        System.out.println(p.nome);
+    }
+}
+
+/*
+* Nesse caso, se a variável texto for declarada dentro de uma classe, como membro, o valor padrão null será atribuído a ela
+*
+* */
---
+++ src/exemplosDevMedia/date_array_e_string/topico04_7/PessoaTesteII.java
@@ -0,0 +1,9 @@
+package exemplosDevMedia.date_array_e_string.topico04_7;
+
+public class PessoaTesteII {
+
+    public static void main(String[] args) {
+        String nome;
+//        System.out.println(nome);
+    }
+}
