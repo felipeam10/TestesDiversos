d5cb8fab183c51fd579014fccf5d46a34c569c1b
felipeam10 <felipeam10@hotmail.com>
2023-10-19 10:57:20 -0300

    aula178 - tudo no programa principal

---
+++ src/exerciciosNelioAlves/secao15_tratamento_de_excecoes/aula178/application/Program.java
@@ -0,0 +1,49 @@
+package exerciciosNelioAlves.secao15_tratamento_de_excecoes.aula178.application;
+
+import exerciciosNelioAlves.secao15_tratamento_de_excecoes.aula178.entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.print("Initial Balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw:");
+        double amount = sc.nextDouble();
+
+        if (amount > acc.getWithdrawLimit()) {
+            System.out.println("Withdraw error: The amount exceeds withdraw limit");
+        } else if (amount > acc.getBalance()) {
+            System.out.println("Withdraw error: Not enough balance");
+        } else {
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+        }
+
+
+        sc.close();
+    }
+
+}
