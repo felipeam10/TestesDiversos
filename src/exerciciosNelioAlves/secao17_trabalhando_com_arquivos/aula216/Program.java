3d0126d63da5fcb741235ac6d7cf70b27a91fd50
felipeam10 <felipeam10@hotmail.com>
2023-10-31 14:25:37 -0300

    aula216 - Bloco try-with-resources

---
+++ src/exerciciosNelioAlves/secao17_trabalhando_com_arquivos/aula216/Program.java
@@ -0,0 +1,28 @@
+package exerciciosNelioAlves.secao17_trabalhando_com_arquivos.aula216;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+        String path = "C:\\Users\\DELL\\workspace-projects\\TestesDiversos\\src\\resources\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
