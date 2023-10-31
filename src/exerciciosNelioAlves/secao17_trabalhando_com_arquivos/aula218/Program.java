b2d103500b5f2a76baaa85238db3fe6ffeae9a24
felipeam10 <felipeam10@hotmail.com>
2023-10-31 14:59:26 -0300

    aula218 - Manipulando pastas com File

---
+++ src/exerciciosNelioAlves/secao17_trabalhando_com_arquivos/aula218/Program.java
@@ -0,0 +1,42 @@
+package exerciciosNelioAlves.secao17_trabalhando_com_arquivos.aula218;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+//        C:\\Users\\DELL\\workspace-projects\\TestesDiversos\\src\\resources\\out.txt"
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        // Vai ler todas as pastas do diretório que for inserido
+        File folders[] = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+
+        // Vai ler todos os arquivos que estão em determinada pasta
+        File files[] = path.listFiles(File::isFile);
+        System.out.println("FILES: ");
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        // Vai criar uma subpasta a partir da pasta inserida
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
+
+        sc.close();
+    }
+}
