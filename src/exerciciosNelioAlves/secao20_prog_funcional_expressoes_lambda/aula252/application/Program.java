20bcc03590c326ef1aff862ce358926556981c78
felipeam10 <felipeam10@hotmail.com>
2023-11-10 14:30:41 -0300

    aula252 - utilizando uma classe anonima dentro do programa principal

--- src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula252/application/Program.java
+++ src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula252/application/Program.java
@@ -4,6 +4,7 @@ import exerciciosNelioAlves.secao20_prog_funcional_expressoes_lambda.aula252.ent
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -15,7 +16,14 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparator());
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
 
         for (Product p : list) {
             System.out.println(p);
