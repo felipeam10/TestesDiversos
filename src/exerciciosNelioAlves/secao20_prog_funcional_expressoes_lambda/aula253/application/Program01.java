d43d425ad20a822d8e4fd907d5d2851257111e7d
felipeam10 <felipeam10@hotmail.com>
2023-11-14 01:47:28 -0300

    aula253 - Programação funcional e cálculo lambda

---
+++ src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula253/application/Program01.java
@@ -0,0 +1,19 @@
+package exerciciosNelioAlves.secao20_prog_funcional_expressoes_lambda.aula253.application;
+
+import java.util.Arrays;
+
+public class Program01 {
+    public static int globalValue = 3;
+    public static void main(String[] args) {
+        int[] vect = new int[] {3, 4, 5};
+        changeOddValues(vect);
+        System.out.println(Arrays.toString(vect));
+    }
+    public static void changeOddValues(int[] numbers) {
+        for (int i=0; i<numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i] += globalValue;
+            }
+        }
+    }
+}
---
+++ src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula253/application/Program03.java
@@ -0,0 +1,17 @@
+package exerciciosNelioAlves.secao20_prog_funcional_expressoes_lambda.aula253.application;
+
+import exerciciosNelioAlves.secao20_prog_funcional_expressoes_lambda.aula253.Entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program03 {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+        list.sort((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
+        list.forEach(System.out::println);
+    }
+}
