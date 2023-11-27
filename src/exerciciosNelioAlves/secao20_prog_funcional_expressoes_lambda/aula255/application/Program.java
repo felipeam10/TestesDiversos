98fdbf307ff3b3541e99a97c9bb12239a8e00539
felipeam10 <felipeam10@hotmail.com>
2023-11-27 16:48:15 -0300

    Declared lambda expression

--- src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula255/application/Program.java
+++ src/exerciciosNelioAlves/secao20_prog_funcional_expressoes_lambda/aula255/application/Program.java
@@ -5,6 +5,7 @@ import exerciciosNelioAlves.secao20_prog_funcional_expressoes_lambda.aula255.uti
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
 
@@ -16,7 +17,9 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 100;
+
+        list.removeIf(pred);
         for (Product p: list) {
             System.out.println(p);
         }
